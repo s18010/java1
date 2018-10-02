797c66a13a77b05d17e9dca51c6d24f89d9bb998
s18010 <s18010@std.it-college.ac.jp>
2018-10-02 08:17:24 +0000

    10/2の課題(hello,world)

---
+++ 1002/Hello.java
@@ -0,0 +1,11 @@
+public class Hello {
+	public static void main(String[] args) {
+	int i = 1;
+	String space = "";
+	while (i <= 5) {
+		System.out.println(space + "Hello, World!");
+		i = i + 1;
+		space = space + " ";	
+		}
+	}
+}
