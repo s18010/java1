7687069c7a83f614b6a06174c8a4d43357f0f144
s18010 <s18010@std.it-college.ac.jp>
2018-10-18 07:18:58 +0000

    7時間目　確認テスト

---
+++ 1018/En7_1.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class En7_1 {
+    public static void main(String[] args) {
+        List<Integer> numbers = new ArrayList<Integer>();
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(1);
+        numbers.add(2);
+
+        System.out.println(numbers);
+    }}
---
+++ 1018/En7_2.java
@@ -0,0 +1,15 @@
+import java.util.Set;
+import java.util.TreeSet;
+
+public class En7_2 {
+    public static void main(String[] args) {
+    Set<Integer> numbers = new TreeSet<>();
+
+    numbers.add(2);
+    numbers.add(3);
+    numbers.add(1);
+    numbers.add(2);
+
+    System.out.println(numbers);
+    }
+}
