82e650eacc15d8a224960b18d7eab18b1d43e1be
s18010 <s18010@std.it-college.ac.jp>
2018-10-24 01:15:12 +0000

    8時間目

---
+++ 1023/Main.java
@@ -0,0 +1,63 @@
+import java.io.*;
+
+
+public class Main {
+    private static final String LINE_SEPARATOR = System.lineSeparator();
+
+    public static String readFromFile(String fileName) {
+        File file = new File(fileName);
+        StringBuilder sb = new StringBuilder();
+
+        FileReader fr = null;
+        BufferedReader br = null;
+        try {
+            fr = new FileReader(file);
+            br = new BufferedReader(fr);
+            String line;
+        while ((line = br.readLine()) != null) {
+            sb.append(line).append(LINE_SEPARATOR);
+        }
+        return sb.toString();
+        } catch (FileNotFoundException e) {
+            System.err.println(String.format("ファイルが見つかりません: %s", fileName));
+            return null;
+        } catch (IOException e) {
+            System.err.println(String.format("ファイル読み込みに失敗しました: %s", fileName));
+            return null;
+        } finally {
+            closeSilently(br);
+            closeSilently(fr);
+        }
+    }
+
+     public static void main(String[] args) {
+        final String fileName = "Test.txt";
+        if (writeToFile(fileName, readFromFile("Main.java"))) {
+           System.out.println(String.format("ファイルの書き込みに成功しました: %s", fileName));
+        } else {
+            System.out.println(String.format("ファイルへの書き込みに失敗しました: %s", fileName));
+        }
+   }
+
+   private static boolean writeToFile(String fileName, String context) {
+    File file = new File(fileName);
+    try (
+            FileWriter fw = new FileWriter(file);
+            BufferedWriter bw = new BufferedWriter(fw);
+            PrintWriter pw = new PrintWriter(bw);
+            ) {
+            pw.println(context);
+            return true;
+    } catch (IOException e) {
+        return false;
+    }
+   }
+
+    private static void closeSilently(AutoCloseable target) {
+        if (target != null) {
+            try {
+                target.close();
+            } catch (Exception e) {}
+        }
+    }
+}
